e1ec59e7fd6f5b5b1a8342ddb3efca07095eeb9c
Will Corcoran <wcorcoran@ucsb.edu>
2022-11-15 11:31:19 -0800

    first quick implementation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# https://data.nasdaq.com/tools/api
 import nasdaqdatalink
+
 nasdaqdatalink.ApiConfig.api_key = "JEg17RDmuJqLE6KY-WvN"
 mydata = nasdaqdatalink.get_table('ZACKS/FC', ticker='AAPL')
 
